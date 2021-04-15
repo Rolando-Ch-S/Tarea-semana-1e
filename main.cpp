909a35cb006b5f7c68fce1197960213518adcfa0
Rolando Chavarria <>
2021-04-15 22:16:36 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,16 @@
+/* 5. Calcular la cantidad de segundos que le toma a la luz viajar del sol a Marte y mostrar el
+resultado en pantalla. 
+ */
+#include <iostream>
+using namespace std;
+
+int main() {
+ float n1, Total;
+  n1 = 28; //Tiempo en minutos
+  Total = n1 * 60; //Tiempo en segundos
+
+    cout << "\nCalcula la cantidad de segundos que le toma a la luz viajar del sol a Marte: ";//función del Prográma
+
+    cout << "La cantidad de segundos que le toma a la luz viajar del sol a Marte es: "  <<Total <<"seg";
+    return 0;
+}
